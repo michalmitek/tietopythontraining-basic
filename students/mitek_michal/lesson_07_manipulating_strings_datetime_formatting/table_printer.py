c3cb7251348f9360c7b33133ce0e864de8757690
Michal Mitek <mitek.michal@gmail.com>
2018-06-14 19:56:26 +0200

    Add tablePrinter propsal solution

--- students/mitek_michal/lesson_07_manipulating_strings_datetime_formatting/table_printer.py
+++ students/mitek_michal/lesson_07_manipulating_strings_datetime_formatting/table_printer.py
@@ -1,6 +1,24 @@
 
 def print_table():
-    pass
+
+    tableData=[['apples', 'oranges', 'cherries', 'banana'],
+            ['Alice', 'Bob', 'Carol', 'David'],
+            ['dogs', 'cats', 'moose', 'goose']]
+
+    newTable = {0:[], 1:[], 2:[], 3:[]}
+
+    for li in tableData:
+        for i in range(len(li)):
+            newTable[i].append(li[i])
+
+    longest = 0
+    for key, value in newTable.items():
+        length = len(''.join(value))
+        if length > longest:
+            longest = length
+
+    for key, value in newTable.items():
+        print(' ' * (longest - len(''.join(value))) + ' '.join(value))
 
 
 if __name__ == '__main__':
